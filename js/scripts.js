69eced51b7e52da9a826ca60e04edb6076f21875
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-27 22:54:52 -0600

    Add or condition for height

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $("#user-info").submit(function(event) {
     event.preventDefault();
     var age = parseInt($("#user-age").val());
-    var height = parseInt($("user-height").val());
+    var height = $("#user-height").val();
 
-    if (age >= 16) {
+    if (age >= 16 || height >= "5") {
       $("#older-taller-ride").show();
       $("#baby-ride").hide();
     } else {
-      alert("Sorry! You're not old enough. But here's something we think you can handle...lol");
+      alert("Sorry! You're not old enough or tall enough. But here's something we think you can handle...lol");
       $("#older-taller-ride").hide();
       $("#baby-ride").show();
     }
